49968786fdb22842c1d14314f621cac86d75ac6d
Kai-Abrorbek <abror0dev@gmail.com>
2025-11-06 09:44:18 +0900

    feat: create members schema related  logic

---
+++ apps/lunotel-api/src/libs/dto/member/member.input.ts
@@ -0,0 +1,124 @@
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsEmail, IsIn, IsNotEmpty, IsOptional, Length, Min } from 'class-validator';
+import { MemberStatus, MemberType } from '../../enums/member.enum';
+import { availableAgentSorts, availableMembersSorts } from '../../config';
+import { Direction } from '../../enums/common.enum';
+
+@InputType()
+export class SignupInput {
+	@IsNotEmpty()
+	@Length(5, 12)
+	@Field(() => String)
+	memberNick: string;
+
+	@IsNotEmpty()
+	@Field(() => String)
+	memberPhone: string;
+
+	@IsNotEmpty()
+	@Length(5, 12)
+	@Field(() => String)
+	memberPassword: string;
+
+	@IsNotEmpty()
+	@IsEmail()
+	@Field(() => String)
+	memberEmail: string;
+
+	@IsOptional()
+	@Field(() => MemberType, { nullable: true })
+	memberType: MemberType;
+
+	@IsOptional()
+	@Field(() => MemberStatus, { nullable: true })
+	memberStatus: MemberStatus;
+}
+
+@InputType()
+export class LoginInput {
+	@IsNotEmpty()
+	@Length(3, 12)
+	@Field(() => String)
+	memberNick: string;
+
+	@IsNotEmpty()
+	@Length(5, 12)
+	@Field(() => String)
+	memberPassword: string;
+}
+
+@InputType()
+class AIsearch {
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	text?: string;
+}
+
+@InputType()
+export class AgentsInquiry {
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	page: number;
+
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	limit: number;
+
+	@IsOptional()
+	@IsIn(availableAgentSorts)
+	@Field(() => String, { nullable: true })
+	sort?: string;
+
+	@IsOptional()
+	@Field(() => Direction, { nullable: true })
+	direction?: Direction;
+
+	@IsNotEmpty()
+	@Field(() => AIsearch, { nullable: true })
+	search?: AIsearch;
+}
+
+/** ADMIN **/
+
+@InputType()
+class MIsearch {
+	@IsOptional()
+	@Field(() => MemberType, { nullable: true })
+	memberType?: MemberType;
+
+	@IsOptional()
+	@Field(() => MemberStatus, { nullable: true })
+	memberStatus?: MemberStatus;
+
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	text?: string;
+}
+
+@InputType()
+export class MembersInquiry {
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	page: number;
+
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	limit: number;
+
+	@IsOptional()
+	@IsIn(availableMembersSorts)
+	@Field(() => String, { nullable: true })
+	sort?: string;
+
+	@IsOptional()
+	@Field(() => Direction, { nullable: true })
+	direction?: Direction;
+
+	@IsNotEmpty()
+	@Field(() => MIsearch, { nullable: true })
+	search?: MIsearch;
+}
---
+++ apps/lunotel-api/src/libs/dto/member/member.update.ts
@@ -0,0 +1,49 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, Length } from 'class-validator';
+import { MemberStatus, MemberType } from '../../enums/member.enum';
+import { ObjectId } from 'mongoose';
+
+@InputType()
+export class MemberUpdate {
+	@IsNotEmpty()
+	@Field(() => String)
+	_id: ObjectId;
+
+	@IsOptional()
+	@Length(3, 12)
+	@Field(() => String, { nullable: true })
+	memberNick?;
+
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	memberPhone?;
+
+	@IsOptional()
+	@Length(5, 12)
+	@Field(() => String, { nullable: true })
+	memberPassword?;
+
+	@IsOptional()
+	@Field(() => MemberType, { nullable: true })
+	memberType?;
+
+	@IsOptional()
+	@Field(() => MemberStatus, { nullable: true })
+	memberStatus?;
+
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	memberFullName?;
+
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	memberImage?;
+
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	memberAddress?;
+
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	memberDesc?;
+}
