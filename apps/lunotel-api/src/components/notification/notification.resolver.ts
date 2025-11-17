1bfeaf1f6d603901f3b72fc754056cf2523838f2
Kai-Abrorbek <abror0dev@gmail.com>
2025-11-17 13:08:28 +0900

    fix: modify notification resolver

--- apps/lunotel-api/src/components/notification/notification.resolver.ts
+++ apps/lunotel-api/src/components/notification/notification.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { NotificationService } from './notification.service';
 import { Notification } from '../../libs/dto/notification/notification';
 import { UseGuards } from '@nestjs/common';
@@ -7,10 +7,21 @@ import { NotificationUpdateInput } from '../../libs/dto/notification/notificatio
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { NotificationInput } from '../../libs/dto/notification/notification.input';
 
 @Resolver()
 export class NotificationResolver {
 	constructor(private readonly notificationService: NotificationService) {}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => Notification)
+	public async createNotification(
+		@Args('input') input: NotificationInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Notification> {
+		return await this.notificationService.createNotification(input);
+	}
 
 	@UseGuards(WithoutGuard)
 	@Query(() => Notification)
